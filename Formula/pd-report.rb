ac1c1c2735c1dd905afe400e61e8e6929d3fa74b
rogersole <roger.sole@gmail.com>
2018-07-13 15:50:29 +0200

    Brew formula update for pd-report version v0.1.0

--- Formula/pd-report.rb
+++ Formula/pd-report.rb
@@ -3,7 +3,7 @@ class PdReport < Formula
   homepage "https://github.com/rogersole/go-pagerduty-oncall-report/"
   url "https://github.com/rogersole/go-pagerduty-oncall-report/releases/download/v0.1.0/pd-report_0.1.0_darwin_amd64.tar.gz"
   version "0.1.0"
-  sha256 "086d2151e634c5732904f840513c0859364fc95b53f09c4be5440cdf23a27ce7"
+  sha256 "5e7c9a3878893c99329c8ea16f4d10230ee44be28fcbbe558b65c40eeb84c304"
 
   def install
     bin.install "pd-report"
